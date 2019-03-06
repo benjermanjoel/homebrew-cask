7cb1d8e681ca676742100384b9f11c8614a38b55
Benjamin Julian <benjermanjoel@outlook.com>
2019-03-06 10:12:41 -0600

    Add jamf-pro-server-tools-gui

--- Casks/jamf-pro-server-tools-gui.rb
+++ Casks/jamf-pro-server-tools-gui.rb
@@ -1,10 +1,10 @@
-cask 'jamf-pro-server-tools-gui' do
+cask 'jamf-pro-server-tools-gui.rb' do
   version ':latest'
   sha256 '4c4c08038cc25073fcc2d940684944a6ea094f37bf2f684f74020d0e12199bd5'
 
   url 'https://archive.services.jamfcloud.com/jamf-pro-server-tools/release/latest/gui/server-tools.jar'
   name 'server-tools.jar'
-  homepage 'https://archive.services.jamfcloud.com/jamf-pro-server-tools/'
+  homepage 'https://archive.services.jamfcloud.com/#jamf-pro-server-tools/release/latest/'
 
-  artifact 'server-tools.jar', target: '/Library/JSS/bin/server-tools.jar'
+  artifact 'server-tools.jar', target: "/Library/JSS/bin/server-tools.jar"
 end
